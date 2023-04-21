ebcf5b6bc3bdd7d3632bb16766564092e0632235
Rmlyy <hello@rmly.dev>
2023-04-21 14:17:54 +0300

    decrease cache duration

--- etc/cache.js
+++ etc/cache.js
@@ -1,6 +1,6 @@
 const NodeCache = require('node-cache')
 
-const ping = new NodeCache({ stdTTL: 10 })
-const traceroute = new NodeCache({ stdTTL: 60 })
+const ping = new NodeCache({ stdTTL: 5 })
+const traceroute = new NodeCache({ stdTTL: 5 })
 
 module.exports = { ping, traceroute }
